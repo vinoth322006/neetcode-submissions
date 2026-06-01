338203dda4764873386da9166f22d368ea86b3c0
VINOTH P <133226329+vinoth322006@users.noreply.github.com>
2026-06-01 18:12:18 +0530

    Add: duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=0;j<nums.length;j++){
+                if(i!=j && nums[i]==nums[j]){
+                    return true;
+                }
+            }
+        }
+    return false;
+    }
+}
